8f841216ad1669db0480f6ab908f891587629c52
Bader Sanad Alanazi <alanazibs@rcjy.gov.sa>
2024-02-11 10:30:41 +0300

    Initial commit

---
+++ src/test/java/com/bader88/docker/demo/DockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bader88.docker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
